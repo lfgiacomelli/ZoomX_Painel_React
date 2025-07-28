5246b973ccc33279b1f1b7cc52e3b2c556d1815f
lfgiacomelli <lfgiacomellirodrigues@gmail.com>
2025-07-28 20:13:53 -0300

    [fix] correção de rotas

--- src/components/layout/HomeHeader.tsx
+++ src/components/layout/HomeHeader.tsx
@@ -42,7 +42,7 @@ export default function HomeHeader() {
                 Entrar
               </a>
 
-              <a onClick={() => navigate('/email')}>
+              <a onClick={() => navigate('/e-mail')}>
                 <button
                   type="button"
                   className="relative inline-flex items-center rounded-md bg-gradient-to-r from-blue-600 to-blue-500 px-4 py-2 text-sm font-medium text-white shadow-md
